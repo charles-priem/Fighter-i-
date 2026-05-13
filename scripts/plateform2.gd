b71ddbcc5e12a130b5eae901a6875ca9919ef7ff
fau-c <cousin.faustine.mala@gmail.com>
2026-05-13 16:07:29 +0200

    plateformes bougeantes

---
+++ scripts/plateform2.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#MAX_POSITION_LEFT = 200
+	#MAX_POSITION_RIGHT = 700
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	pass
